05bfa358358cb2476cdf3a75956ff484efae2ebb
Teddy Grandsire <teddy@grandsire.org>
2018-05-06 13:33:55 +0200

    Game Player: UniqueNamedPlayerPerPlayListener

---
+++ src/AppBundle/EventListener/Game/UniqueNamedPlayerNamePerPlayListener.php
@@ -0,0 +1,32 @@
+<?php
+namespace AppBundle\EventListener\Game;
+
+use Doctrine\ORM\Event\OnFlushEventArgs;
+
+use AppBundle\Entity\Game\Play\Player\NamedPlayer;
+
+class UniqueNamedPlayerNamePerPlayListener
+{
+	/**
+     * Pre persist listener based on doctrine common.
+     *
+     * @param OnFlushEventArgs $args
+     */
+    public function onFlush(OnFlushEventArgs $args)
+    {
+    	$uow = $args->getEntityManager()->getUnitOfWork();
+
+    	foreach($uow->getScheduledEntityInsertions() as $entity) {
+	        if ($entity instanceof NamedPlayer) {
+	            foreach ($entity->getPlay()->getPlayers() as $player) {
+	            	if ($entity->getName() == $player->getName()) {
+	        			$em = $args->getEntityManager();
+
+	        			$em->detach($entity);
+	        			throw new \Exception('Player name must unique for a play');
+	            	}
+	            }
+	        }
+    	}
+    }
+}
