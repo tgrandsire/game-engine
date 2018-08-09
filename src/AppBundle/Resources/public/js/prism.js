981a4645d2ae6e75d53c5cf16faa7b6f8723b859
Teddy Grandsire <teddy@grandsire.org>
2018-08-09 23:30:11 +0200

    Website firsts steps, Symfony Encore (foundation, prism)

---
+++ src/AppBundle/Resources/public/js/prism.js
@@ -0,0 +1,5 @@
+var Prism = require('prismjs/components/prism-core.js');
+require('prismjs/plugins/line-numbers/prism-line-numbers.js');
+var loadLanguages = require('prismjs/components/index.js');
+loadLanguages(['php', 'css']);
+
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    // directory where all compiled assets will be stored
+    .setOutputPath('web/build/')
+
+    // what's the public path to this directory (relative to your project's document root dir)
+    .setPublicPath('/build')
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    // will output as web/build/app.js
+    .addEntry('prism', [
+        './src/AppBundle/Resources/public/js/prism.js',
+        './node_modules/prismjs/themes/prism.css',
+        './node_modules/prismjs/plugins/line-numbers/prism-line-numbers.css',
+        './src/AppBundle/Resources/public/sass/prism.scss'
+    ])
+
+    // will output as web/build/global.css
+    .addStyleEntry('website', './src/AppBundle/Resources/public/sass/foundation.scss')
+
+
+    // allow sass/scss files to be processed
+    .enableSassLoader()
+
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+
+    .enableSourceMaps(!Encore.isProduction())
+
+    // create hashed filenames (e.g. app.abc123.css)
+    //.enableVersioning()
+;
+
+// export the final configuration
+module.exports = Encore.getWebpackConfig();
